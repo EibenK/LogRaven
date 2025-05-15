4646e7c58725331485c857ab1ebdb5170a4d6f2e
Kevin Eiben <kevineiben20@gmail.com>
2025-05-15 02:43:59 -0700

    aligns test functions with actual methods

--- tests/plugins_test/agents_information_test.py
+++ tests/plugins_test/agents_information_test.py
@@ -2,12 +2,21 @@ import unittest
 
 class AgentServicesInformationTest(unittest.TestCase):
 
-    def test_case_1(self):
+    def test_acquire_logs(self):
+        print("Not implemented yet")
+
+    def test_init_subclass(self):
+        print("Not implemented yet")
+
+    def test_deregister(self):
+        print("Not implemented yet")
+
+    def test_get_registered(self):
         print("Not implemented yet")
 
 class AgentServiceLogFinder(unittest.TestCase):
 
-    def test_case_1(self):
+    def test_acquire_logs(self):
         pass
 
 if __name__ == "__main__":
